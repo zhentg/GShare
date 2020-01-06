cd5dd3893d48e8f8340b9511466881df4ed26c1d
Zhenting Gao <40551969+zhentg@users.noreply.github.com>
2020-01-06 18:17:25 +0800

    Update syncPdb.sh

--- PDB/syncPdb.sh
+++ PDB/syncPdb.sh
@@ -5,3 +5,7 @@
 
 cd /data/databases
 rsync -rlpt -v -z --delete --port=33444 rsync.rcsb.org::ftp_data/structures/divided/pdb/ ./pdb
+
+#add this script to Cron table is necessary, here is how to do that
+#crontab -e
+#0 0,12 * * * zhentg /home/zhentg/bin/rsync_pdb_utility.sh
